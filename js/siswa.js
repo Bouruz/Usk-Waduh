c5a36580f11e38dc77aaaba8f69a2a911bb1812f
Bouruz <nurafit2006@gmail.com>
2025-05-07 02:50:18 +0700

    update

---
+++ js/siswa.js
@@ -0,0 +1,13 @@
+const dataSiswa = {
+    nis: "12345678",
+    nama: "Dewi Lestari",
+    kelas: "XII RPL 1",
+    tempatLahir: "Jakarta",
+    tanggalLahir: "2007-01-10",
+    kelamin: "Perempuan",
+    agama: "Islam",
+    alamat: "Jl. Mawar No.2, Jakarta",
+    citaCita: "Programmer",
+    foto: "img/siswa1.jpg"
+  };
+  
